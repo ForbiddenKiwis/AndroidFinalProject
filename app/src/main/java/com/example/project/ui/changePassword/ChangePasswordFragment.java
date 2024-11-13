3c4d52fbf2e463ced9a36c44717ca7359fdcdbae
ForbiddenKiwis <jerwinson.cunanan19@gmail.com>
2024-11-12 22:45:17 -0500

    Back end for Update User password works

--- app/src/main/java/com/example/project/ui/changePassword/ChangePasswordFragment.java
+++ app/src/main/java/com/example/project/ui/changePassword/ChangePasswordFragment.java
@@ -1,33 +1,92 @@
 package com.example.project.ui.changePassword;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.project.R;
 import com.example.project.databinding.FragmentChangePasswordBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChangePasswordFragment extends Fragment {
 
-private FragmentChangePasswordBinding binding;
+    private ChangePasswordViewModel changePasswordViewModel;
+    private FragmentChangePasswordBinding binding;
+
+    private int userId;
+    private DatabaseReference userDB;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyAppPrefs",
+                MODE_PRIVATE);
+        userId = sharedPreferences.getInt("userId", -1);
+
+        if (userId == -1)
+            Log.e("UpdateInfoFragment", "No User Id found in sharedPreferences");
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        ChangePasswordViewModel changePasswordViewModel =
+        changePasswordViewModel =
                 new ViewModelProvider(this).get(ChangePasswordViewModel.class);
 
-    binding = FragmentChangePasswordBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentChangePasswordBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        changePasswordViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        userDB = FirebaseDatabase.getInstance().getReference("User").child(String.valueOf(userId));
+
+        changePasswordViewModel.loadUserData(userId);
+
+        changePasswordViewModel.getUserId().observe(getViewLifecycleOwner(), id -> {
+            binding.tvEditUserUserId.setText(userId != -1 ? String.valueOf(id) : "");
+        });
+
+        Button btnEditUserBack = binding.btnEditUserBack;
+        btnEditUserBack.setOnClickListener(v -> goHome());
+
+        Button btnEditUserSave = binding.btnEditUserSave;
+        btnEditUserSave.setOnClickListener(v -> save());
+
         return root;
     }
 
-@Override
+    private void save() {
+        String password = binding.etUserPassword.getText().toString().trim();
+
+        userDB.child("password").setValue(password);
+
+        Toast.makeText(getActivity(), "Person Data Save!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void goHome() {
+        NavController navController = Navigation.findNavController(requireActivity(),
+                R.id.nav_host_fragment_content_main_menu);
+        navController.navigate(R.id.nav_home);
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
--- app/src/main/java/com/example/project/ui/changePassword/ChangePasswordViewModel.java
+++ app/src/main/java/com/example/project/ui/changePassword/ChangePasswordViewModel.java
@@ -1,19 +1,50 @@
 package com.example.project.ui.changePassword;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ChangePasswordViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<Integer> userId = new MutableLiveData<>();
+    private final  MutableLiveData<String> password = new MutableLiveData<>();
+    private DatabaseReference userDB;
 
     public ChangePasswordViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        userDB = FirebaseDatabase.getInstance().getReference("User");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void loadUserData(int id){
+        userDB.child(String.valueOf(id)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    userId.setValue(snapshot.child("userId").getValue(Integer.class));
+                    password.setValue(snapshot.child("age").getValue(String.class));
+
+                } else {
+                    Log.e("ChangePasswordViewModel", "No Data Available");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("ChangePasswordViewModel", "Database Error: "+ error.getMessage());
+            }
+        });
     }
+
+    public LiveData<Integer> getUserId() {
+        return userId;
+    }
+    public LiveData<String> getPassword() {return password;}
 }
