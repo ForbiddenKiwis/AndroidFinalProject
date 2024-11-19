cf54058438fed0429b63ff000431c93bc6a8d88f
ForbiddenKiwis <jerwinson.cunanan19@gmail.com>
2024-11-18 23:26:27 -0500

    Added a Graph widget to be added in the home fragment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
         gradlePluginPortal()
     }
 }
@@ -15,7 +14,6 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
     }
 }
 
