87c88bf25ed27651adfaeaf2ecca37bd04934124
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-12-10 11:12:44 -0500

    The user's weigth and date of entry are saved to the database

--- app/src/main/java/com/example/project/userweight/AddUserWeightActivity.java
+++ app/src/main/java/com/example/project/userweight/AddUserWeightActivity.java
@@ -1,6 +1,9 @@
 package com.example.project.userweight;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.project.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class AddUserWeightActivity extends AppCompatActivity {
+public class AddUserWeightActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAddUserWeight, btnViewUserWeight;
+    EditText edId, edWeight;
+    DatabaseReference UserWeightDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,45 @@ public class AddUserWeightActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initialize();
+    }
+
+
+    private void initialize(){
+        btnAddUserWeight = findViewById(R.id.btnAddUserWeight);
+        btnViewUserWeight = findViewById(R.id.btnViewUserWeight);
+        edId = findViewById(R.id.edAddUserWeightId);
+        edWeight = findViewById(R.id.edAddUserWeight);
+
+        btnViewUserWeight.setOnClickListener(this);
+        btnAddUserWeight.setOnClickListener(this);
+        this.UserWeightDatabase = FirebaseDatabase.getInstance().getReference("UserWeight");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btnAddUserWeight) Add();
+
+        if (id == R.id.btnViewUserWeight) ViewUserWeight();
+    }
+
+    private void Add(){
+        int weight = Integer.valueOf(edWeight.getText().toString().trim());
+        int id = Integer.valueOf(edId.getText().toString().trim());
+        UserWeight userWeight = new UserWeight(weight,id);
+        UserWeightDatabase.push().setValue(userWeight);
+        clearToken();
+    }
+
+    private void clearToken(){
+        edWeight.setText("");
+        edId.setText("");
+    }
+
+    private void ViewUserWeight(){
+
     }
 }
